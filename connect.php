358853e1847abc0ab7d0c779854802f99a31f4c1
Stan Brat <stan.brat@icloud.com>
2021-12-02 10:20:21 +0000

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,4 @@
 <?php
 // db onnection
-$conn = mysqli_connect('localhost', 'HNCWEBMR20', 'RTuPGALTD9', 'HNCWEBMR20')
+$conn = mysqli_connect('localhost', '******', '******', '******')
 or die('Could not connect to database');
